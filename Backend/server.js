63b35ebcf7331c8b2ab343113f4ccae30706107a
Keshavcodes3 <Keshavcodes0@gmail.com>
2026-05-29 21:26:04 +0530

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 // Trigger restart
